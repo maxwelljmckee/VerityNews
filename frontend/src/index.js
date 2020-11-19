c3bbfb56da5f955af0de6885d823b66f5d16f5ec
maxwelljmckee <maxwelljmckee@gmail.com>
2020-11-19 11:01:01 -0700

    initialize store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import configureStore from './store/index';
+
+
+const store = configureStore();
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+const Root = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  )
+}
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
