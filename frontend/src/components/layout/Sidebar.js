26a3b6f12dacbc76214e16ddef92c1c44b5938d0
maxwelljmckee <maxwelljmckee@gmail.com>
2021-02-05 17:14:39 -0700

    add meet-dev frontent component to sidebar

--- frontend/src/components/layout/Sidebar.js
+++ frontend/src/components/layout/Sidebar.js
@@ -6,6 +6,50 @@ import Logo from '../welcome/Logo';
 import ChannelHeader from './channels/ChannelHeader';
 
 
+
+const MeetDev = () => {
+  const [show, setShow] = useState(false);
+
+  const toggleShow = () => {
+    setShow(!show)
+  }
+
+  return (
+    <div className='meet-dev__container'>
+      { show ?
+        <div onClick={toggleShow} className='meet-dev__header'>
+          <i className="fas fa-angle-right"></i>
+          Meet the Developer!
+        </div>
+        :
+        <>
+          <div onClick={toggleShow} className='meet-dev__header'>
+            <i className="fas fa-angle-down"></i>
+            Meet the Developer!
+          </div>
+          <div className='meet-dev__info fade-in'>
+            <img src='https://ca.slack-edge.com/T03GU501J-U0173CXKZGD-cdadb0380e42-512' alt='portrait' />
+            <div className='name'>Maxwell McKee</div>
+            <div className='meet-dev__links'>
+              <a target='_blank' href='https://mmckee-dev.com/'>
+                <div className='portfolio'>Portfolio</div>
+              </a>
+              <a target='_blank' href='https://www.linkedin.com/in/maxwell-mckee-385599191/'>
+                <div className='linkedin'>linkedIn</div>
+              </a>
+              <a target='_blank' href='https://angel.co/u/maxwell-mckee'>
+                <div className='angellist'>AngelList</div>
+              </a>
+            </div>
+          </div>
+        </>
+      }
+    </div>
+  )
+}
+
+
+
 const Sidebar = () => {
   const user = useSelector(state => state.session.user)
 
@@ -16,9 +60,12 @@ const Sidebar = () => {
           <Link to='/explore'><Logo /></Link>
         </div>
         { user && <h2 className='welcome__message'>Welcome {user.username}</h2>}
-        <ChannelHeader />
-        <div className='board__container'>
-          
+        <div className='sidebar__items'>
+          <ChannelHeader />
+          <div className='board__container'>
+
+          </div>
+          <MeetDev />
         </div>
       </div>
     </Fragment>
